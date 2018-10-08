40b39b3db2d3f4212f809619178fae3574216b78
Kenton Lam <kenton_lam@outlook.com>
2018-10-08 14:34:18 +1000

    Main application.

---
+++ src/main/game/MainApplication.java
@@ -0,0 +1,11 @@
+package game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class MainApplication extends Application {
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
