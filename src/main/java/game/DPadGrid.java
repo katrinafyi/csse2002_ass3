f33bb2d372ec14d826eac1da116ae15756a89d77
Kenton Lam <kenton_lam@outlook.com>
2018-10-11 15:48:43 +1000

    Button arrows.

--- src/main/java/game/DPadGrid.java
+++ src/main/java/game/DPadGrid.java
@@ -3,17 +3,33 @@ package game;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DPadGrid extends UniformGridPane {
+    private static final Map<Pair<Integer, Integer>, String> labels = new HashMap<>();
+
+    static {
+        labels.put(new Pair<>(0, -1), "↑");
+        labels.put(new Pair<>(0, 1), "↓");
+        labels.put(new Pair<>(-1, 0), "←");
+        labels.put(new Pair<>(1, 0), "→");
+    }
+
     public DPadGrid() {
         super(3, 3);
     }
 
     protected Node generateCell(int col, int row) {
         if (Math.abs(col-1) == Math.abs(row-1)) {
+            // Do not add buttons to corners or the centre.
             return null;
         }
-        Button b = new Button("<--");
+
+        Button b = new Button(labels.get(new Pair<>(col-1, row-1)));
+        b.setStyle("font-size: 20pt; font-weight: bold;");
         Utilities.setMaxWidthHeight(b);
         b.prefHeightProperty().bind(b.widthProperty());
         return b;
