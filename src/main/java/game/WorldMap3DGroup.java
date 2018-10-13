473aa26cccea4e5770843126fdf59d9fef622f3f
Kenton Lam <kenton_lam@outlook.com>
2018-10-13 10:42:26 +1000

    Multiple textures.

--- src/main/java/game/WorldMap3DGroup.java
+++ src/main/java/game/WorldMap3DGroup.java
@@ -3,13 +3,19 @@ package game;
 import csse2002.block.world.Position;
 import csse2002.block.world.Tile;
 import csse2002.block.world.WorldMap;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Orientation;
 import javafx.geometry.Point3D;
 import javafx.scene.AmbientLight;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SceneAntialiasing;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.SubScene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -18,6 +24,9 @@ import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Shape3D;
 import javafx.scene.shape.TriangleMesh;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -107,8 +116,15 @@ public class WorldMap3DGroup extends Group {
         float w = B;
         TriangleMesh mesh = new TriangleMesh();
         mesh.getTexCoords().addAll(
+                // Side textures are left half of image.
                 0f, 0f,
                 0f, 1f,
+                0.5f, 1f,
+                0.5f, 0f,
+
+                // Top textures are right half.
+                0.5f, 0f,
+                0.5f, 1f,
                 1f, 1f,
                 1f, 0f
         );
@@ -130,8 +146,8 @@ public class WorldMap3DGroup extends Group {
 
         mesh.getFaces().addAll(
                 // Top face (xy plane).
-                4,0, 0,1, 5,3,
-                5,3, 0,1, 1,2,
+                4,4, 0,5, 5,7,
+                5,7, 0,5, 1,6,
 
                 // Side faces.
                 0,0, 3,1, 1,3,
@@ -154,7 +170,9 @@ public class WorldMap3DGroup extends Group {
         MeshView meshView = new MeshView(mesh);
 
         PhongMaterial phong = new PhongMaterial();
-        phong.setDiffuseMap(new Image(getClass().getResourceAsStream("/tnt_side.png")));
+        phong.setDiffuseMap(
+tileImages(                new Image(getClass().getResourceAsStream("/tnt_side.png")),
+        new Image(getClass().getResourceAsStream("/tnt_top.png"))));
         //meshView.setMaterial(phong);
         meshView.setMaterial(phong);
         meshView.setTranslateX(0);
@@ -162,6 +180,12 @@ public class WorldMap3DGroup extends Group {
         meshView.setTranslateZ(0);
 
         return meshView;
+    }
+
+    private Image tileImages(Image image1, Image image2) {
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(new ImageView(image1), new ImageView(image2));
+        return hbox.snapshot(new SnapshotParameters(), null);
     }
 
     private Shape3D generateShape(Color color) {
