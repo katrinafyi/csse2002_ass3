09f3d5182ba3cc10cba152c83684bb563bcef73b
Kenton Lam <kenton_lam@outlook.com>
2018-10-17 16:26:55 +1000

    Working on move builder.

--- src/main/java/game/GameWorldMapView.java
+++ src/main/java/game/GameWorldMapView.java
@@ -1,23 +1,19 @@
 package game;
 
 import csse2002.block.world.TooLowException;
-import csse2002.block.world.WorldMap;
 import game.model.EventDispatcher;
 import game.model.events.BaseBlockWorldEvent;
 import game.model.events.BlocksChangedEvent;
 import game.model.events.BuilderMovedEvent;
 import game.model.events.ErrorEvent;
-import game.model.events.WorldMapEvent;
 import game.model.events.WorldMapLoadedEvent;
 import game.model.BlockType;
 import game.view.TileView;
 import csse2002.block.world.Position;
-import javafx.animation.PauseTransition;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
-import javafx.util.Duration;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +21,7 @@ import java.util.Map;
 public class GameWorldMapView {
 
     private final UniformGridPane gridPane;
-    private final Map<Position, TileView> tileSquareMap = new HashMap<>();
+    private final Map<Position, TileSquare> tileSquareMap = new HashMap<>();
     private Position currentPosition;
 
     public GameWorldMapView(EventDispatcher<BaseBlockWorldEvent> controller) {
@@ -33,28 +29,28 @@ public class GameWorldMapView {
         gridPane.setPrefWidth(500);
 
         controller.addListener(WorldMapLoadedEvent.class, this::worldMapLoadedHandler);
-        controller.addListener(BuilderMovedEvent.class, this::setPositionHandler);
+        controller.addListener(BuilderMovedEvent.class, this::builderMovedHandler);
         controller.addListener(ErrorEvent.class, this::errorHandler);
-        controller.addListener(BlocksChangedEvent.class, this::tileBlocksHandler);
+        controller.addListener(BlocksChangedEvent.class, this::blocksChangedHandler);
         controller.addListener(null, this::allHandler);
 
         this.gridPane.setMaxWidth(Control.USE_PREF_SIZE);
 
         this.gridPane.setMaxHeight(Control.USE_PREF_SIZE);
 
-                this.gridPane.prefWidthProperty().addListener(
-                (a, o, n) -> {
-                    for (Node tile : this.gridPane.getChildren()) {
-                        ((Region) tile).setMaxWidth(((double) n - (gridPane.COLUMNS - 1) * gridPane.GAP) / gridPane.COLUMNS);
-                    }
-                });
+        this.gridPane.prefWidthProperty().addListener(
+        (a, o, n) -> {
+            for (Node tile : this.gridPane.getChildren()) {
+                ((Region) tile).setMaxWidth(((double) n - (gridPane.COLUMNS - 1) * gridPane.GAP) / gridPane.COLUMNS);
+            }
+        });
     }
 
     private void allHandler(BaseBlockWorldEvent e) {
         System.out.println(e);
     }
 
-    private void tileBlocksHandler(BlocksChangedEvent event) {
+    private void blocksChangedHandler(BlocksChangedEvent event) {
         try {
             TileView tile = tileSquareMap.get(event.getPosition());
             int height = event.getTile().getBlocks().size();
@@ -75,9 +71,35 @@ public class GameWorldMapView {
         System.out.println(event);
     }
 
-    private void setPositionHandler(BaseBlockWorldEvent e) {
-        BuilderMovedEvent event = (BuilderMovedEvent) e;
+    private void builderMovedHandler(BuilderMovedEvent event) {
+        Position oldPosition = currentPosition;
+
+        // Change in column index.
+        int dc = event.getDirection().asPosition().getX();
+        // Change in row index.
+        int dr = event.getDirection().asPosition().getY();
+
+        for (int c = 0; c < gridPane.COLUMNS; c++) {
+            for (int r = 0; r < gridPane.ROWS; r++) {
+                // Position index of the current cell.
+                Position pos = new Position(
+                        currentPosition.getX()+c,
+                        currentPosition.getY()+r);
+                TileSquare tile = tileSquareMap.get(pos);
+                if (tile == null) {
+                    continue;
+                }
+
+
+            }
+        }
+
         currentPosition = event.getNewPosition();
+    }
+
+    private boolean onGrid(int col, int row) {
+        return (0 <= col && col < gridPane.COLUMNS)
+                && (0 <= row && row < gridPane.ROWS);
     }
 
     private void worldMapLoadedHandler(WorldMapLoadedEvent event) {
