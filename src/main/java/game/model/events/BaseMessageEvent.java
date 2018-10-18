6de7b0ac5b35f6c43a5d2bea8dd1cfd32152283e
Kenton Lam <kenton_lam@outlook.com>
2018-10-18 15:54:28 +1000

    Comments on message events.

--- src/main/java/game/model/events/BaseMessageEvent.java
+++ src/main/java/game/model/events/BaseMessageEvent.java
@@ -1,12 +1,23 @@
 package game.model.events;
 
+/**
+ * Superclass for all messaging events.
+ */
 public abstract class BaseMessageEvent extends BaseBlockWorldEvent {
     private final String message;
 
+    /**
+     * Create a new event with the given message.
+     * @param message Message.
+     */
     public BaseMessageEvent(String message) {
         this.message = message;
     }
 
+    /**
+     * Gets the event's message.
+     * @return Message string.
+     */
     public String getMessage() {
         return message;
     }
--- src/main/java/game/model/events/MessageEvent.java
+++ src/main/java/game/model/events/MessageEvent.java
@@ -1,6 +1,13 @@
 package game.model.events;
 
+/**
+ * A non-error message event.
+ */
 public class MessageEvent extends BaseMessageEvent {
+    /**
+     * Construct a new message event.
+     * @param message Message string.
+     */
     public MessageEvent(String message) {
         super(message);
     }
